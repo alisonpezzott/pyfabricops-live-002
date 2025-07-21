3dabbe889a07bc105c4509056b955c688584281c
alisonpezzott <alisonpezzott@gmail.com>
2025-07-20 21:37:16 -0300

    Deployed v0.1.0

--- scripts/deploy_powerbi.py
+++ scripts/deploy_powerbi.py
@@ -32,6 +32,14 @@ pf.export_all_semantic_models(
     excluded_starts=('Main',)
 )
 
+# Export semantic models parameters
+pf.extract_semantic_models_parameters(
+    project_path=project_path,
+    workspace_alias=workspace_alias,
+    config_path=config_path,
+    branch=branch,
+)
+
 pf.deploy_all_reports_cicd(
     project_path=project_path,
     workspace_alias=workspace_alias,
